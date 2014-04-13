b4d8a79eda50e3982d22598fdac1793da2c29261
Oleksii Gryniuk (neuton) <mr.neuton@gmail.com>
2014-04-13 23:28:16 +0300

    added some tests with symbols

--- test.c
+++ test.c
@@ -10,6 +10,15 @@ void test(char s[], real v)
 		printf("failed %s = %g\n", s, r);
 }
 
+void tests(char s[], char sx[], real x[], real v)
+{
+	real r = evals(s, sx, x);
+	if (r == v)
+		puts("passed");
+	else
+		printf("failed %s = %g\n", s, r);
+}
+
 int main()
 {
 	test("0" "0", 0);
@@ -27,5 +36,10 @@ int main()
 	test("14+10*(3-4)/(5+5)", 13);
 	test("1+(2*10)*(3-4)-1", -20);
 	test("1+(10-((9*2)))*(3-4)-8 + 3*2^3 - 20 + 5", 10);
+	puts("-----------------------------");
+	real x[] = {1, 2, 3, 4, 5};
+	tests("x + y", "xy", x, 3);
+	tests("x + yz", "xyz", x, 7);
+	tests("(x+y)/z + 2w", "xyzw", x, 9);
 	return 0;
 }
