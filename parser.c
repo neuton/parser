20df3dd1c2072b2e53ac9f90368a776609ff440d
Oleksii Gryniuk (neuton) <mr.neuton@gmail.com>
2014-04-13 23:14:59 +0300

    now respects operations priority

--- parser.c
+++ parser.c
@@ -1,80 +1,43 @@
 #include "parser.h"
 
 #include <stddef.h>
-//#include <stdbool.h>
 #include <math.h>
 
-/*int processstring(char s[], bool doshift)
+static short prio(char op)
 {
-    int i=0, c=0;
-    void shiftstring(int k)
-    {
-         int n=0;
-         while (s[n++]); n++;
-         while ((--n)>=k) s[n]=s[n-1];
-    }
-    void processstring1()
-    {
-         bool t=false;
-         int a=0;
-         do
-               if (s[i]=='(') { i++; processstring1(); }
-               else if (s[i]==')') { if (t) { if (doshift) { shiftstring(a); s[a]='('; i++; shiftstring(i); s[i]=')'; i++; } c+=2; } return; }
-               else if ((a>0)&&((s[i]=='*')||(s[i]=='/')||(s[i]=='^'))) t=true;
-               else if ((s[i]=='-')||(s[i]=='+')||s[i]==0) { if (t) { if (doshift) { shiftstring(a); s[a]='('; i++; shiftstring(i); s[i]=')'; i++; } c+=2; t=false; } a=i+1; }
-         while (s[i++]);
-    }
-    void processstring2()
-    {
-         bool t=false;
-         int a=0;
-         do
-               if (s[i]=='(') { i++; processstring2(); }
-               else if (s[i]==')') { if (t) { if (doshift) { shiftstring(a); s[a]='('; i++; shiftstring(i); s[i]=')'; i++; } c+=2; } return; }
-               else if ((s[i]=='^')&&(a>0)) t=true;
-               else if ((s[i]=='-')||(s[i]=='+')||(s[i]=='*')||(s[i]=='/')||s[i]==0)
-               { if (t) { if (doshift) { shiftstring(a); s[a]='('; i++; shiftstring(i); s[i]=')'; i++; } c+=2; t=false; } a=i+1; if ((s[i]=='-')||(s[i]=='+')) a=0; }
-         while (s[i++]);
-    }
-    while (s[i++]) if ((s[i]=='(')&&(((s[i-1]>='0')&&(s[i-1]<':'))||(s[i-1]=='x'))) { if (doshift) { shiftstring(i); s[i]='*'; i++; } c++; } i=0;
-    processstring1(); i=0;
-    processstring2();
-	return c;
-}*/
+	if ((op=='+')||(op=='-')) return 1;
+	else if ((op=='*')||(op=='/')) return 2;
+	else if (op=='^') return 3;
+}
 
-static real evals_(char s[], char sx[], real x[], unsigned* ishift)
+static real evals_(char s[], char sx[], real x[], unsigned* ishift, real init_r, char init_a, short p)
 {
-	real r=0, n=0, k;
+	real r=init_r, n=0, k;
 	unsigned d=0, j, i=0;
-	char a='+', c=s[0];
+	char a=init_a, c=s[0];
 	if (!c) return NAN;
 	while (1)
 	{
-		if ((c>='0')&&(c<='9'))
+		if (c==' ');
+		else if ((c>='0')&&(c<='9'))
 		{
 			if (d>0) n+=(real)(c-'0')/(d*=10);
 			else n=n*10+(real)(c-'0');
 		}
-		/*else if (s[i]=='y')
-		{
-			for (t=0; (s[++i]>='0')&&(s[i]<='9'); t=t*10+s[i]-'0');
-			i--;
-			n=n==0?y[t]:n*y[t];
-		}*/
 		else if ((c=='.')||(c==','))
 			d=1;
 		else if (c=='(')
-			{ i++; n=evals_(s+i, sx, x, &i); }
+			{ i++; n=evals_(s+i, sx, x, &i, 0, '+', 0); }
 		else if ((c=='s')&&(s[i+1]=='i')&&(s[i+2]=='n')&&(s[i+3]=='('))
-			{ i+=4; n=sin(evals_(s+i, sx, x, &i)); }
+			{ i+=4; n=sin(evals_(s+i, sx, x, &i, 0, '+', 0)); }
 		else if ((c=='c')&&(s[i+1]=='o')&&(s[i+2]=='s')&&(s[i+3]=='('))
-			{ i+=4; n=cos(evals_(s+i, sx, x, &i)); }
+			{ i+=4; n=cos(evals_(s+i, sx, x, &i, 0, '+', 0)); }
 		else if ((c=='t')&&(s[i+1]=='g')&&(s[i+2]=='('))
-			{ i+=3; n=tan(evals_(s+i, sx, x, &i)); }
+			{ i+=3; n=tan(evals_(s+i, sx, x, &i, 0, '+', 0)); }
 		else if ((c=='l')&&(s[i+1]=='n')&&(s[i+2]=='('))
-			{ i+=3; n=log(evals_(s+i, sx, x, &i)); }
+			{ i+=3; n=log(evals_(s+i, sx, x, &i, 0, '+', 0)); }
 		else if ((c=='e')&&(s[i+1]=='x')&&(s[i+2]=='p')&&(s[i+3]=='('))
-			{ i+=4; n=exp(evals_(s+i, sx, x, &i)); }
+			{ i+=4; n=exp(evals_(s+i, sx, x, &i, 0, '+', 0)); }
 		else if ((c=='p')&&(s[i+1]=='i'))
 			{ i++; n=M_PI; }
 		else if (c=='e')
@@ -88,20 +51,18 @@ static real evals_(char s[], char sx[], real x[], unsigned* ishift)
 				n=n==0?x[j]:n*x[j];
 			else
 			{
-				switch (a)
-				{
-					case '+': r+=n; break;
-					case '-': r-=n; break;
-					case '*': r*=n; break;
-					case '/': r/=n; break;
-					case '^': r=pow(r, n);
-				}
-				//printf("%f ", r);
+				if (((c=='+')||(c=='-')||(c=='/')||(c=='*')||(c=='^'))&&(prio(c)>prio(a)))
+					{ i++; n=evals_(s+i, sx, x, &i, n, c, prio(a)); c=s[i]; }
+				if (a=='+') r+=n;
+				else if (a=='-') r-=n;
+				else if (a=='*') r*=n;
+				else if (a=='/') r/=n;
+				else if (a=='^') r=pow(r, n);
 				if ((!c)||(c==')'))
 					{ *ishift+=i; return r; }
 				else if ((c=='+')||(c=='-')||(c=='/')||(c=='*')||(c=='^'))
-					a=c;
-				else if (c!=' ')
+					{ if (prio(c)<=p) { *ishift+=i; return r; } a=c; }
+				else
 					{ *ishift+=i; return NAN; }
 				n=0.; d=0;
 			}
@@ -113,7 +74,7 @@ static real evals_(char s[], char sx[], real x[], unsigned* ishift)
 extern real evals(char s[], char sx[], real x[])
 {
 	unsigned i=0;
-	return evals_(s, sx, x, &i);
+	return evals_(s, sx, x, &i, 0, '+', 0);
 }
 
 extern real eval(char s[])
